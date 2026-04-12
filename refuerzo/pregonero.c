7d1e309217715f37f20f21fa8651919ac83e1a75
hernn-dev <hernn.dev@gmail.com>
2026-04-12 12:58:20 -0600

    Nivel 3- Reto 1: Pregonero funcional y verificado con -Wextra y pedantic

---
+++ refuerzo/pregonero.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//1. El Prototipo
+void imprimirLogo();
+
+int main(void) {
+	//2.LLAMADA DE LA FUNCIÓN
+	imprimirLogo();
+
+
+	printf("Iniciando procesos de arquitectura...\n");
+
+	imprimirLogo();
+	return 0;
+}
+
+//3. DEFINICIÓN DE LA FUNCIÓN  (EL   TRABAJO REAL)
+void imprimirLogo() {
+
+printf("\n*****************************************\n");
+printf(" * SISTEMA DE LOGOS DE HERNAN v1.0   *\n");
+printf("\n*****************************************\n\n");
+}
